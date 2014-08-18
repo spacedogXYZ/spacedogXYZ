93d026142f1c8d5c7634135fa63b0fcd76d169ad
Josh Levinger <josh@levinger.net>
2014-08-17 21:51:07 -0700

    console rollover imagemap +responsive

--- js/spacedog.js
+++ js/spacedog.js
@@ -1,16 +1,31 @@
 function resetOverlay() {
   console.log('resetOverlay');
   var offset = $('#splash #stars').height();
+  var fullWidth = $('#splash #stars').width();
   $('#splash .overlay').each(function(index) {
     $(this).css({top: -1*(index+1)*offset});
   });
   $('#splash').css({height:offset});
+  $('#rollover').attr({height: offset, width: fullWidth});
 }
+
 
 $(document).ready(function() {
   console.log('spacedog');
 
   $(window).resize(resetOverlay);
   resetOverlay();
+
+  $('img[usemap]').rwdImageMaps();
+
+  // bind rollover events
+  $('area').on('mouseover', function() {
+    $('img#rollover').attr({src: '/static/splash/'+$(this).attr('alt')+'.png'});
+  });
+  $('area').on('mouseout', function() {
+    $('img#rollover').attr({src: '/static/splash/transparent.png'});
+  });
+
+
 });
 
