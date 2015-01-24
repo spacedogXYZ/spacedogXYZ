b28b82a02c5366b03b1c40227177b7d95c227459
Josh Levinger <josh@levinger.net>
2015-01-24 15:43:27 -0800

    blastoff form

--- js/spacedog.js
+++ js/spacedog.js
@@ -1,14 +1,25 @@
 function resize() {
-  var fullWidth = Number($('.fullwidth').width())
-    + Number($('.fullwidth').css('padding-left').split('px')[0]*2);
-    //hackery because jQuery doesn't compute width with padding
-  var fullHeight = (fullWidth / 1400) * 900;
-  $('.fullwidth').css({height: fullHeight});
+  try {
+    var fullWidth = Number($('.fullwidth').width())
+      + Number($('.fullwidth').css('padding-left').split('px')[0]*2);
+      //hackery because jQuery doesn't compute width with padding
+    var fullHeight = (fullWidth / 1400) * 900;
+    $('.fullwidth').css({height: fullHeight});
+  } catch(err) {
+
+  }
 }
 
 
 $(document).ready(function() {
-  console.log('Spacedog');
+  var spacedog = '\n' +
+  ' _______                            __               \n'+
+  '|     __|.-----.---.-.----.-----.--|  |.-----.-----. \n'+
+  '|__     ||  _  |  _  |  __|  -__|  _  ||  _  |  _  | \n'+
+  '|_______||   __|___._|____|_____|_____||_____|___  | \n'+
+  '         |__|                                |_____| \n'+
+  '\n';
+  console.log(spacedog);
 
   $(window).resize(resize);
   resize();
