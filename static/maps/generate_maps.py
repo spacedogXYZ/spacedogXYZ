63a1664b046f02b7a4dfbb4a7165639d8888af1e
Josh Levinger <josh@levinger.net>
2015-02-19 15:04:28 -0800

    add location circle to svg maps

--- static/maps/generate_maps.py
+++ static/maps/generate_maps.py
@@ -1,11 +1,13 @@
 from kartograph import Kartograph
+import csv
 
-COUNTRIES = {
-    'palestine': 'PSE',
-    'usa': 'USA',
-    'uganda': 'UGA',
-    'libya': 'LBY',
-    'peru': 'PER'
+MAPS = {
+    'palestine': {'iso':'PSE', 'location': {'Ramallah': [31.9, 35.2]}},
+    'oakland': {'iso':'USA', 'location': {'Oakland': [37.804444, -122.270833]}},
+    'hollyhock': {'iso':'CAN', 'location': {'Cortes Island': [50.116667, -124.966667]}},
+    'uganda': {'iso':'UGA', 'location': {'Kampala': [0.313611, 32.581111]}},
+    'libya': {'iso':'LBY', 'location': {'Tripoli': [32.904722, 13.193333]}},
+    'peru': {'iso':'PER', 'location': {'Tarapoto': [-6.483333, -76.366667]}},
 }
 
 config = {
@@ -13,6 +15,9 @@ config = {
         "country": {
             "src": "natural_earth/ne_10m_admin_0_countries_lakes.shp",
             "simplify": 5,   
+        },
+        "location": {
+            "src": "tmp.csv",
         }
     },
     "bounds": {
@@ -30,8 +35,17 @@ config = {
 if __name__ == "__main__":
     K = Kartograph()
     css = open('map.css').read()
-    for (country, iso) in COUNTRIES.items():
-        config['layers']['country']['filter'] = (lambda r: r['ISO_A3'] == iso)
+
+    for (country,m) in MAPS.items():
+        config['layers']['country']['filter'] = (lambda r: r['ISO_A3'] == m['iso'])
+
+        #write location dict to tmp.csv
+        with open('tmp.csv', 'wb') as csvfile:
+            writer =  csv.DictWriter(csvfile, fieldnames=['name','lat','lon'])
+            writer.writeheader()
+            for name,loc in m['location'].items():
+                writer.writerow({'name': name, 'lat': loc[0], 'lon': loc[1]})
+
         fn = 'svg/%s.svg' % country
         K.generate(config, outfile=fn, stylesheet=css)
         print "generated", fn
