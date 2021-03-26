9001936ef2e3db4a029bd4eea331ce5d3810b266
Josh Levinger <josh@levinger.net>
2021-03-26 16:41:03 -0700

    use netlify forms for blastoff submissions
    remove formspree

--- js/blastoff.js
+++ js/blastoff.js
@@ -3,10 +3,8 @@ $('form').submit(function(event) {
   $("form").validate({
     submitHandler: function(form) {
       $.ajax({
-        url: "//formspree.io/josh@spacedog.xyz", 
         method: "POST",
         data: $('form').serializeArray(),
-        dataType: "json",
         success: function() { 
           $('form').slideUp();
           $('#success').slideDown();
